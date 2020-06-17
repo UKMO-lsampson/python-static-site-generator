5d5aa9da351fae5c472bfa5af50e669890a35329
lewis.sampson <lsampson@vld240.cmpd1.metoffice.gov.uk>
2020-06-17 11:23:03 +0100

    Completed module

--- ssg.py
+++ ssg.py
@@ -4,9 +4,13 @@ from ssg.site import Site
 
 def main(source="content", dest="dist"):
     config = {
-        "source": source,
-        "dest": dest,
-        "parsers": [ ssg.parsers.ResourceParser(), ],
+            "source": source,
+            "dest": dest,
+            "parsers": [
+                ssg.parsers.ResourceParser(),
+                ssg.parsers.MarkdownParser(),
+                ssg.parsers.ReStructuredTextParser(),
+            ],
         }
     Site(**config).build()
 
